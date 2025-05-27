1b4daabfb7108a277bc19f5cd93d664dbb91376b
insaecula <morozzzik228@gmail.com>
2025-05-27 06:27:09 +0300

    пакетики

--- src/main/java/ru/hogwarts/school/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/school/controller/FacultyController.java
@@ -1,20 +1,49 @@
 package ru.hogwarts.school.controller;
-
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.model.Student;
+import java.util.List;
 import model.Faculty;
 import model.Student;
+import org.springframework.stereotype.Service;
+
+
 import ru.hogwarts.school.service.FacultyService;
-
-import java.util.List;
-
+@RestController
+@RequestMapping("/faculty")
 public class FacultyController {
+
+    private final FacultyService facultyService;
+
+    public FacultyController(FacultyService facultyService) {
+        this.facultyService = facultyService;
+    }
+
     @GetMapping("/filter")
     public List<Faculty> getFacultyByNameOrColor(@RequestParam String value) {
-         facultyService;
         return facultyService.findByNameOrColorIgnoreCase(value);
     }
 
     @GetMapping("/{id}/students")
     public List<Student> getStudentsOfFaculty(@PathVariable Long id) {
         return facultyService.getStudentsByFacultyId(id);
+    }
+    @PostMapping
+    public Faculty createFaculty(@RequestBody Faculty faculty) {
+        return facultyService.createFaculty(faculty);
+    }
+
+    @GetMapping("/{id}")
+    public Faculty getFacultyById(@PathVariable Long id) {
+        return facultyService.getFacultyById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Faculty updateFaculty(@PathVariable Long id, @RequestBody Faculty faculty) {
+        return facultyService.updateFaculty(id, faculty);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteFaculty(@PathVariable Long id) {
+        facultyService.deleteFaculty(id);
     }
 }
---
+++ src/test/java/ru/hogwarts/school/FacultyControllerTest.java
@@ -0,0 +1,35 @@
+package ru.hogwarts.school;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import ru.hogwarts.school.controller.FacultyController;
+import ru.hogwarts.school.model.Faculty;
+import ru.hogwarts.school.service.FacultyService;
+
+import static java.lang.reflect.Array.get;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(FacultyController.class)
+public class FacultyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FacultyService facultyService;
+
+    @Test
+    public void testGetFacultyById() throws Exception {
+        Faculty faculty = new Faculty(1L, "Gryffindor", "Red");
+        Mockito.when(facultyService.getFacultyById(1L)).thenReturn(faculty);
+
+        mockMvc.perform(get("/faculty/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name").value("Gryffindor"));
+    }
+}
